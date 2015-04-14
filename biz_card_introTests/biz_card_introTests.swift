656e41f5865cccdc499b15886f678cae2ba1e3ab
Admin <pypcompany@MacBook-Air.local>
2015-04-13 19:55:28 -0700

    Initial Commit

---
+++ biz_card_introTests/biz_card_introTests.swift
@@ -0,0 +1,36 @@
+//
+//  biz_card_introTests.swift
+//  biz_card_introTests
+//
+//  Created by Admin on 4/13/15.
+//  Copyright (c) 2015 Peter Park. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class biz_card_introTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
